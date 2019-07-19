2c21f005326f7f4c68c9e15f298bd913dc0c3335
Vasiliy V. Garshin <50753438+vgarshin@users.noreply.github.com>
2019-07-19 16:13:12 +0300

    Update rkn_scrap_smb.sh

--- rkn_scrap_smb.sh
+++ rkn_scrap_smb.sh
@@ -8,14 +8,13 @@ CACHE_DIR="cache_rkn_scraping_smb"
 CACHE_DIR_XML="cache_rkn_scraping_smb_xml"
 DATA_DIR="_data"
 EMAIL="vgarshin@yandex.ru"
-CONT_NAME="parcesites_rkn"
-ACTUAL_DATE="2019-02"
+CONT_NAME="parcesites_rkn_iter"
 
 sudo mkdir "../$CACHE_DIR_XML"
 sudo mkdir "../$CACHE_DIR"
 
 #python rkn_scrap_smb.py _data 20190718 cache_rkn_scraping_smb vgarshin@yandex.ru 2019-02 cache_rkn_scraping_smb_xml
-sudo docker run -i --name $CONT_NAME $VOLUMES extradata python -u rkn/rkn_scrap_smb.py $DATA_DIR $PROC_DATE $CACHE_DIR $EMAIL $ACTUAL_DATE $CACHE_DIR_XML
+sudo docker run -i --name $CONT_NAME $VOLUMES extradata python -u rkn/rkn_scrap_smb_iter.py $DATA_DIR $PROC_DATE $CACHE_DIR $EMAIL $CACHE_DIR_XML
 sudo docker rm -f $CONT_NAME
 
 sudo rm -r "../$CACHE_DIR_XML"
